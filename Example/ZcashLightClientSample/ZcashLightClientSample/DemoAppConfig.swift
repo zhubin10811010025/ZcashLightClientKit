875fda24b099705fedb0a36c68f5425c7945c9b7
Francisco Gindre <francisco.gindre@gmail.com>
2020-06-25 18:35:30 -0300

    Add new testnet seed and checkpoint

--- Example/ZcashLightClientSample/ZcashLightClientSample/DemoAppConfig.swift
+++ Example/ZcashLightClientSample/ZcashLightClientSample/DemoAppConfig.swift
@@ -12,9 +12,9 @@ import MnemonicSwift
 struct DemoAppConfig {
     static var host = ZcashSDK.isMainnet ? "lightwalletd.z.cash" : "lightwalletd.testnet.z.cash"
     static var port: Int = 9067
-    static var birthdayHeight: BlockHeight = ZcashSDK.isMainnet ? 663174 : 620_000
+    static var birthdayHeight: BlockHeight = ZcashSDK.isMainnet ? 663174 : 950000
     static var network = ZcashSDK.isMainnet ? ZcashNetwork.mainNet : ZcashNetwork.testNet
-    static var seed = ZcashSDK.isMainnet ? Mnemonic.deterministicSeedBytes(from: "still champion voice habit trend flight survey between bitter process artefact blind carbon truly provide dizzy crush flush breeze blouse charge solid fish spread")! : Array("testreferencealicetestreferencealice".utf8)
+    static var seed = ZcashSDK.isMainnet ? Mnemonic.deterministicSeedBytes(from: "still champion voice habit trend flight survey between bitter process artefact blind carbon truly provide dizzy crush flush breeze blouse charge solid fish spread")! : Mnemonic.deterministicSeedBytes(from: "come exhibit fatal kid consider useless pigeon glove crawl stumble crunch left click labor curtain debris park hour raise wonder guilt upset eager order")!
     static var address: String {
         "\(host):\(port)"
     }
